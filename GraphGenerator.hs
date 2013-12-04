89efb81d9f722bec08bb9663b26164de8b2a4df5
David Luposchainsky <dluposchainsky@gmail.com>
2013-12-04 16:19:54 +0100

    Compiles

--- GraphGenerator.hs
+++ GraphGenerator.hs
@@ -1,17 +1,21 @@
 import System.Environment
 import Data.List
 import Data.Maybe
+import Control.Monad
 import Text.Printf
 import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Data.Foldable as F
+import qualified Data.Traversable as T
+import Data.Monoid
+import Text.Read
 
 -- | Letters in the MIU alphabet.
 --
 --   Although not necessary in the book since all words start with "M" and have
 --   no other "M", it is encoded here explicitly to make the program more
 --   flexible.
-data MUIAtom = M | I | U
+data MiuAtom = M | I | U
       deriving (Eq, Ord, Show)
 
 -- | Used to tag reduction steps.
@@ -21,22 +25,22 @@ data Rule = MX  -- ^ Mx   -> Mxx  ("duplicate")
           | III -- ^ xIIIy -> xUy ("convert triple I")
       deriving (Eq, Ord, Show)
 
-newtype MuiExpr = MuiExpr [MUIAtom]
+newtype MiuExpr = MiuExpr [MiuAtom]
       deriving (Eq, Ord, Show)
 
 
 
--- | Calculate all "MuiExpr"s that can be obtained by applying one rule once.
+-- | Calculate all "MiuExpr"s that can be obtained by applying one rule once.
 --
 --   The result may contain duplicate entries, for example reducing "U""U""U"
 --   yields one entry for eliminating each pair.
-ruleAll :: MuiExpr -> Set (MuiExpr, Rule)
-ruleAll (MuiExpr mui) = (Set.fromList . mapMaybe applyRule . splits) mui
+ruleAll :: MiuExpr -> Set (MiuExpr, Rule)
+ruleAll (MiuExpr mui) = (Set.fromList . mapMaybe applyRule . splits) mui
       where
-            applyRule (xs, I:[])     = Just (MuiExpr (xs ++ [U]            ) , PU)
-            applyRule (xs, U:U:ys)   = Just (MuiExpr (xs ++ ys             ) , UU)
-            applyRule (xs, M:ys)     = Just (MuiExpr (xs ++ [M] ++ ys ++ ys) , MX)
-            applyRule (xs, I:I:I:ys) = Just (MuiExpr (xs ++ [U] ++ ys      ) , III)
+            applyRule (xs, I:[])     = Just (MiuExpr (xs ++ [U]            ) , PU)
+            applyRule (xs, U:U:ys)   = Just (MiuExpr (xs ++ ys             ) , UU)
+            applyRule (xs, M:ys)     = Just (MiuExpr (xs ++ [M] ++ ys ++ ys) , MX)
+            applyRule (xs, I:I:I:ys) = Just (MiuExpr (xs ++ [U] ++ ys      ) , III)
             applyRule _              = Nothing
 
 
@@ -66,9 +70,8 @@ data Graph = Graph (Set Node)
                    (Set Node)
                    (Set Edge)
 
-newtype Node = Node MuiExpr
-      deriving (Eq, Ord)
-data Edge = Edge MuiExpr MuiExpr Rule
+type Node = MiuExpr
+data Edge = Edge Node Node Rule
       deriving (Eq, Ord)
 
 
@@ -76,7 +79,7 @@ data Edge = Edge MuiExpr MuiExpr Rule
 -- | Construct all outgoing edges from a given start node
 growNode :: Node -> Set Edge
 growNode from = Set.map toEdge (ruleAll from)
-      toEdge (miu, rule) = Edge from miu rule
+      where toEdge (miu, rule) = Edge from miu rule
 
 
 -- | Grow each unvisited node in a graph
@@ -86,7 +89,7 @@ growGraph limit (Graph unvisited visited edges) = Graph unvisited' visited' edge
             -- New edges: grow all nodes and flatten the structure
             edges' = (constrainLength . F.fold) (Set.map growNode unvisited)
             constrainLength = Set.filter underLimit
-            underLimit (Edge _ (MuiExpr mui) _) = length mui <= limit
+            underLimit (Edge _ (MiuExpr mui) _) = length mui <= limit
 
             -- Add unvisited nodes to the visited ones
             visited' = unvisited <> visited
@@ -103,7 +106,7 @@ growAll limit g = let g'@(Graph n _ _ ) = growGraph limit g
 
 
 -- | Starting point of the book's puzzle
-gebStartGraph = Graph (Set.singleton (MuiExpr [M,I])) Set.empty Set.empty
+gebStartGraph = Graph (Set.singleton (MiuExpr [M,I])) Set.empty Set.empty
 
 
 -- ==================
@@ -114,13 +117,13 @@ class ToDot a where
       toDot :: a -> String
 
 instance ToDot Graph where
-      toDot (Graph _ _ edges) = Set.map toDot edges
+      toDot (Graph _ _ edges) = F.fold (Set.map toDot edges)
 
 instance ToDot Edge where
       toDot (Edge start end rule) =
             printf "\t%s -> %s %s;\n" (toDot start)
                                       (toDot end)
-                                      (toDot t)
+                                      (toDot rule)
 
 instance ToDot MiuExpr where
       toDot (MiuExpr miu) = concatMap toDot miu
@@ -155,6 +158,7 @@ addBoilerplate body = unlines
       , "}"
       , body
       , "}"
+      ]
 
 -- ==================
 
@@ -162,7 +166,6 @@ addBoilerplate body = unlines
 main :: IO ()
 main = do
       args <- getArgs
-      case readMaybe =<< listToMaybe of
-            Just limit -> do
-                  (putStrLn . addBoiletplate . toDot) (growAll limit gebStartGraph)
+      case readMaybe =<< listToMaybe args of
+            Just limit -> (putStrLn . addBoilerplate . toDot) (growAll limit gebStartGraph)
             _ -> putStrLn "Expecting Int parameter for maximum word length"
